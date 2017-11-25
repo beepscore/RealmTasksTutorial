aea99920192a4d0c7b99d12a9b1329bddbf28bf7
beepscore <support@beepscore.com>
2017-11-25 14:31:44 -0800

    Add model classes Task and TaskList in ViewController.swift. // TODO: consider extract model classes to a separate file

--- RealmTasksTutorial/ViewController.swift
+++ RealmTasksTutorial/ViewController.swift
@@ -8,6 +8,27 @@
 
 import UIKit
 
+import RealmSwift
+
+// TODO: consider extract model classes to a separate file
+
+// MARK: Model
+
+final class TaskList: Object {
+    @objc dynamic var text = ""
+    @objc dynamic var id = ""
+    let items = List<Task>()
+
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
+
+final class Task: Object {
+    @objc dynamic var text = ""
+    @objc dynamic var completed = false
+}
+
 class ViewController: UITableViewController {
 }
 
